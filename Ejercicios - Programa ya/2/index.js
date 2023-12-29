f2be1872dd3d00887e2a2819c9e78ee1df413d84
Frederick-js <faivergarzon@gmail.com>
2023-12-29 01:25:17 -0500

    ejercicio 2

--- Ejercicios - Programa ya/2/index.js
+++ Ejercicios - Programa ya/2/index.js
@@ -4,15 +4,19 @@ desde a hasta b.
 */
 
 function ejercicioDos(a = 0, b = 0) {
-    let nums = [];
-    
-    for (let i = a; i <=b; i++) {
-        
-          if (i % 2 === 0)nums.push(i); 
-    
-    
+  let numsPar = [];
+  let numsImpar = [];
+
+  for (let i = a; i <= b; i++) {
+    if (i % 2 === 0) {
+      numsPar.push(i);
+    } else {
+      numsImpar.push(i);
     }
-    console.log(nums);
+  }
+  console.log(numsPar);
+  console.log(numsImpar);
 }
 
-ejercicioDos(2,8);
+ejercicioDos(2, 8);
+
