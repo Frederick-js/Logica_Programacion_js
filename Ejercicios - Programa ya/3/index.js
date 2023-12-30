090c0da0729758caae5c4c1ea88873ea5bde8522
Frederick-js <faivergarzon@gmail.com>
2023-12-30 00:42:08 -0500

    terminacion ejercicio 3

--- Ejercicios - Programa ya/3/index.js
+++ Ejercicios - Programa ya/3/index.js
@@ -2,16 +2,13 @@
  3) Realizar una tabla de multiplicacion de un numero (a) desde 0 hasta x, pero que no se muestre la multiplicacion de a * 5
 */
 
-function multiplicacion(a = 0, x = 0) {
-    for (let i = a; a <= x; a++) {
-        if (i*a === i*5 ) {
-            console.log('sacamos el 5');
-        }else{
-            console.log(i*a);
-        }
-        
+function ejercicioTres(a = 0, x = 12){
+    let result;
+    for (let i = 0; i <= x; i++){
+        result = (a*i);
+        console.log(`${a} x ${i} = ${result}`);
     }
 }
 
-multiplicacion(2,5);
+ejercicioTres(5);
 
