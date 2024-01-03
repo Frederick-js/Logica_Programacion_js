d6d6201f317b0dce38e98bdee4ba4940a1d1099f
Frederick-js <faivergarzon@gmail.com>
2024-01-03 03:24:04 -0500

    ejercicio 7 termiinado

---
+++ Ejercicios - Programa ya/7/index.js
@@ -0,0 +1,12 @@
+/*
+ 7) Realizar una funcion que, dado como dato de entrada una oracion, nos devuelva un arreglo que contenga
+ todas las palabras usadas en la oracion
+*/
+
+function ejercicioSiete (text = "Hola mundo"){
+ let words = text.split(" ")
+console.log(words);
+}
+
+ejercicioSiete();
+ejercicioSiete('Alejita ospina torin');
