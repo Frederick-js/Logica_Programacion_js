28a5b10a0bacaf69b0f0fca2bf20992de4aaf9ec
Frederick-js <faivergarzon@gmail.com>
2024-01-03 02:55:46 -0500

    ejercicio 6 terminado

--- Ejercicios - Programa ya/6/index.js
+++ Ejercicios - Programa ya/6/index.js
@@ -2,4 +2,11 @@
  6) Hacer una funcion que, dado como dato de entrada nuestra fecha de cumpleanos, nos dia cuantos dias faltan para el mismo.
 */
 
-console.log(Date().);
+function ejercicioSeis(a= "5 feb 2024"){
+    let cumple = new Date(a);
+    let hoy = new Date();
+    let diasRestantes = Math.floor((cumple.getTime()- hoy.getTime()) / (1000 * 60 * 60 * 24))
+    console.log(`Tu cumples es ${cumple} y faltan ${diasRestantes} dias.`);
+}
+
+ejercicioSeis();
