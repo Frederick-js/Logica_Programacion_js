8ec6b72ce83af9fb14c1b4dea617f4e292d86df4
Frederick-js <faivergarzon@gmail.com>
2024-01-04 21:40:29 -0500

    iniciando ejercicio 8

--- Ejercicios - Programa ya/6/index.js
+++ Ejercicios - Programa ya/6/index.js
@@ -2,8 +2,8 @@
  6) Hacer una funcion que, dado como dato de entrada nuestra fecha de cumpleanos, nos dia cuantos dias faltan para el mismo.
 */
 
-function ejercicioSeis(a= "5 feb 2024"){
-    let cumple = new Date(a);
+function ejercicioSeis(a = "5 feb 2024"){
+    let cumple = new Date();
     let hoy = new Date();
     let diasRestantes = Math.floor((cumple.getTime()- hoy.getTime()) / (1000 * 60 * 60 * 24))
     console.log(`Tu cumples es ${cumple} y faltan ${diasRestantes} dias.`);
---
+++ Ejercicios - Programa ya/8/index.js
@@ -0,0 +1,22 @@
+/*
+ 8) Crea 5 objetos y almacenalos en un arreglo, debes hacer una funcion para filtrar a todas las personas que tengan el nombre luis y que esa misma funcion tambien muestre cuales
+ son mayores de edad
+*/
+
+
+
+let Personas = {
+    "Aleja": 26,
+    "Steve": 14,
+    "Mariela": 50,
+    "miguel": 15,
+    "Luis": 19
+};
+
+function filtro() {
+    console.log(Personas);
+}
+
+
+
+filtro();
